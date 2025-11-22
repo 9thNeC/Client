079fd3c96dd685233b56934f04cea0c32bd7d11c
Dubabbi <thdms3160@naver.com>
2025-11-22 22:33:27 +0900

    style: 타이포 1차 설정

--- web/src/pages/main/main-page.tsx
+++ web/src/pages/main/main-page.tsx
@@ -1,9 +1,13 @@
 const Main = () => {
   return (
-    <div>
-      <h1 className="title text-gray-90 flex-row-center mt-10">
-        메인페이지 배포테스트
-      </h1>
+    <div className="text-gray-90 flex-col-center pt-[5rem]">
+      <h1 className="h1">타이포 heading large</h1>
+      <h2 className="h2">타이포 heading medium</h2>
+      <h3 className="h3">타이포 heading small</h3>
+      <br />
+      <p className="b1">타이포 body large</p>
+      <p className="b2">타이포 body medium</p>
+      <p className="b3">타이포 body small</p>
     </div>
   );
 };
