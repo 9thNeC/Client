53178b63341008be12ac0c2d8b82686c4a05224c
Dubabbi <thdms3160@naver.com>
2025-11-22 16:48:36 +0900

    init: tailwindCSS 세팅

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,7 +12,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="title text-gray-90">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
---
+++ web/src/pages/login-page.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div>df</div>;
+};
+
+export default Login;
